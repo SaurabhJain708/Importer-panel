79ffda6689e7d3a3494fddc7db2887bea150c8e6
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-04-27 16:58:16 +0530

    Added remove-category route and modified remove-item route

--- src/app/api/admin/remove-item/route.ts
+++ src/app/api/admin/remove-item/route.ts
@@ -1,37 +1,37 @@
 import { mongoDb } from "@/lib/dbConnect";
+import Item from "@/models/items.model";
 import { ApiError } from "@/lib/ErrorResponse";
 import { ApiResponse } from "@/lib/ApiResponse";
 import { NextRequest, NextResponse } from "next/server";
-import Category from "@/models/category.model";
 
 export async function POST(req: NextRequest) {
   try {
     await mongoDb();
 
-    const { categoryId } = await req.json();
+    const { itemId } = await req.json();
 
-    if (!categoryId) {
+    if (!itemId) {
       return NextResponse.json(
-        new ApiError(400, "Category ID is required"),
+        new ApiError(400, "Item ID is required"),
         { status: 400 }
       );
     }
 
-    const categoryRemoved = await Category.findOneAndDelete({ _id: categoryId });
+    const itemRemoved = await Item.findOneAndDelete({ _id: itemId });
 
-    if (!categoryRemoved) {
+    if (!itemRemoved) {
       return NextResponse.json(
-        new ApiError(404, "Category not found, Deletion failed"),
+        new ApiError(404, "Item not found, Deletion failed"),
         { status: 404 }
       );
     }
 
     return NextResponse.json(
-      new ApiResponse(200, null, "Category removed successfully")
+      new ApiResponse(200, null, "Item removed successfully")
     );
   } catch (error) {
     return NextResponse.json(
-      new ApiError(500, "Internal server error, Category not removed"),
+      new ApiError(500, "Internal server error, Item not removed"),
       { status: 500 }
     );
   }
