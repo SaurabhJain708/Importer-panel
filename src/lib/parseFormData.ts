9d5d2a2fae37868e482c266df81f8d1969a071eb
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-04-27 22:09:45 +0530

    Added add category route

--- src/lib/parseFormData.ts
+++ src/lib/parseFormData.ts
@@ -1,15 +1,23 @@
-import formidable from 'formidable';
-import { IncomingMessage } from 'http';
+import formidable from "formidable";
+import { IncomingMessage } from "http";
+import { NextRequest } from "next/server";
 
-export async function parseFormData(req:any) {
+export async function parseFormData(req: NextRequest) {
   const form = formidable({ multiples: true });
 
-  const nodeReq = req instanceof IncomingMessage ? req : (req?.raw ?? req);
+  // Cast the NextRequest object to IncomingMessage for formidable
+  const nodeReq =
+    req instanceof IncomingMessage ? req : (req as any).raw || req;
 
-  return new Promise((resolve, reject) => {
-    form.parse(nodeReq, (err, fields, files) => {
-      if (err) reject(err);
-      else resolve({ fields, files });
-    });
-  });
+  return new Promise<{ fields: formidable.Fields; files: formidable.Files }>(
+    (resolve, reject) => {
+      form.parse(nodeReq, (err, fields, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ fields, files });
+        }
+      });
+    }
+  );
 }
