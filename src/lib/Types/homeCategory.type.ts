b0d71325292902f890a4c5856d730f0e8d104f7b
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-04-30 17:23:23 +0530

    Added categories section

---
+++ src/lib/Types/homeCategory.type.ts
@@ -0,0 +1,26 @@
+interface item {
+  _id: string;
+  name: string;
+  description: string;
+  coverImage: string;
+  supportingImages?: string[];
+  rating: number;
+  price?: number;
+  currency?: string;
+  stock?: number;
+  isInStock: boolean;
+  category: string;
+  vendor?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface CatType {
+    _id:string;
+  name: string;
+  description?: string;
+  coverImage?: string;
+  parentCategory?: string;
+  items: item[]; // for nested categories
+  createdAt: Date;
+  updatedAt: Date;
+}
